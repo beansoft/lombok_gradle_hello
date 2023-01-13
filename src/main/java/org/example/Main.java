0beeddf485c89dd42be73a5dc9f410521804c535
Jacky Liu <Changjiong.Liu@jetbrains.com>
2023-01-13 17:43:54 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Entity entity = new Entity();
+        entity.setAge(1);
+        System.out.println("Hello world!");
+    }
+}
